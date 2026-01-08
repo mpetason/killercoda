e37d12951afac588c7977703bdbe064a464d802d
Mike Petersen <mike.petersen@loft.sh>
2026-01-07 17:19:31 -0800

    remove verify

--- vCluster-103/step4/verify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Check services and ports for both vClusters"
-
-vcluster connect my-vcluster-a --namespace team-x --command "kubectl get svc my-app -o wide"
-vcluster connect my-vcluster-b --namespace team-x --command "kubectl get svc my-app -o wide"
-
-echo "OK"
